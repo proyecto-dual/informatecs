7e0f4671facc4d3ceef9e193bd591f733655db1c
Aileen Cs <aileenlizetcruzsanchez@MacBook-Air-de-Aileen.local>
2026-03-25 00:08:23 -0700

    noticias supabase

--- src/app/api/noticias/route.js
+++ src/app/api/noticias/route.js
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
 );
 
 // GET - Obtener todas las noticias y eventos
